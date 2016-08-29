edfae73f609838488b5c2d6ae27e60607b681360
gembarrett <gem@gembarrett.com>
2016-08-29 16:15:24 +0100

    basic graphics

---
+++ levelup/RingsView.swift
@@ -0,0 +1,21 @@
+//
+//  RingsView.swift
+//  levelup
+//
+//  Created by Gemma Barrett on 29/08/2016.
+//  Copyright © 2016 Gem Barrett. All rights reserved.
+//
+
+import UIKit
+
+class RingsView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
