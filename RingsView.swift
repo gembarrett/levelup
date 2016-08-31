74c51475c80ac9368627e3c99a82a89f8c3c7128
gembarrett <gem@gembarrett.com>
2016-08-31 20:28:19 +0100

    radius and some calculations

--- RingsView.swift
+++ RingsView.swift
@@ -9,13 +9,25 @@
 import UIKit
 
 class RingsView: UIView {
+    
+    var endRing:CGFloat = 0.0
+    var ringStrokeWidth:CGFloat = 10.0
+    var ringColour = UIColor.yellowColor()
+    var ringBackgroundColour = UIColor.blackColor()
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-        // Drawing code
+        let fullLoop = 2.0 * CGFloat(M_PI)
+        let start:CGFloat = -0.25 * fullLoop
+        let end:CGFloat = endRing * fullLoop + start
+        
+        var middleBit = CGPointMake(CGRectGetMidX(rect), CGRectGetMidY(rect))
+        
+        var radius:CGFloat = 0.0
+        if CGRectGetWidth(rect) > CGRectGetHeight(rect) {
+            radius = (CGRectGetWidth(rect) - ringStrokeWidth) / 2.0
+        } else {
+            radius = (CGRectGetHeight(rect) - ringStrokeWidth) / 2.0
+        }
     }
-    */
 
 }
