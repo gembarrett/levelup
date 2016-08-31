67ce284e0b12dc48d6532a8fc2f7c6ffdacf2389
gembarrett <gem@gembarrett.com>
2016-08-31 20:20:48 +0100

    IBOutlets and actions for yellow

--- levelup/ViewController.swift
+++ levelup/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var percentage: UILabel!
+    @IBOutlet weak var yellowRing: RingsView!
+    @IBAction func yellowSlider(sender: UISlider) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
