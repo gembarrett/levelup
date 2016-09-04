dbeb05407bed48c57a71787609d08dd039533a21
gembarrett <gem@gembarrett.com>
2016-09-04 09:06:00 +0100

    adding labels

--- levelup/ViewController.swift
+++ levelup/ViewController.swift
@@ -17,8 +17,11 @@ class ViewController: UIViewController, UITabBarDelegate {
     @IBOutlet weak var killsTab: UITabBarItem!
     @IBOutlet weak var damageTab: UITabBarItem!
     @IBOutlet weak var pointsTab: UITabBarItem!
-    
     @IBOutlet weak var dataTabs: UITabBar!
+    
+    @IBOutlet weak var scoutLbl: UILabel!
+    @IBOutlet weak var pyroLbl: UILabel!
+    @IBOutlet weak var demomanLbl: UILabel!
     
     func dataTab(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
         dataTabs.selectedItem = killsTab
@@ -47,18 +50,20 @@ class ViewController: UIViewController, UITabBarDelegate {
     override func viewDidLoad() {
         dataTab(dataTabs, didSelectItem: killsTab)
         super.viewDidLoad()
-        
         redRing.ringStrokeWidth = 35.0
         let backgroundRingColour = UIColor(white: 0.15, alpha: 1.0)
         redRing.ringBackgroundColour = backgroundRingColour
+        scoutLbl.textColor = UIColor.redColor()
 
         orangeRing.ringColour = UIColor.orangeColor()
         orangeRing.ringStrokeWidth = 30.0
         orangeRing.ringBackgroundColour = backgroundRingColour
+        pyroLbl.textColor = UIColor.orangeColor()
         
         yellowRing.ringColour = UIColor.yellowColor()
         yellowRing.ringStrokeWidth = 25.0
         yellowRing.ringBackgroundColour = backgroundRingColour
+        demomanLbl.textColor = UIColor.yellowColor()
     }
 
     override func didReceiveMemoryWarning() {
